e126de4e427a0ad383822324cedc01cb0beba135
Aman Rana <amanranahere@gmail.com>
2025-05-06 23:47:40 +0530

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="h-screen bg-[#333] text-white flex justify-center items-center">
+        Its working!
+      </div>
+    </>
+  );
+}
+
+export default App;
