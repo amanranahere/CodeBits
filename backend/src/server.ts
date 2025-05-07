f430cbec4f443ff27d968242e7d560b1e7fb16a0
Aman Rana <amanranahere@gmail.com>
2025-05-07 23:52:07 +0530

    set up mongoDB server

---
+++ backend/src/server.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import app from "./app";
+import connectDB from "./db/db_index";
+
+dotenv.config({ path: "./.env" });
+
+connectDB()
+  .then(() => {
+    const PORT = process.env.PORT || 8000;
+
+    app.listen(PORT, () => {
+      console.log(`Server is running at port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection failed!", err);
+  });
