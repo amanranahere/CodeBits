994075af22372d235aaf98293d6d7aefe339aa33
Aman Rana <amanranahere@gmail.com>
2025-06-06 08:37:43 +0530

    add ui to new file modal

--- frontend/src/components/Modals/FeedbackModal.tsx
+++ frontend/src/components/Modals/FeedbackModal.tsx
@@ -1,5 +1,134 @@
-export default function FeedbakcModal() {
+import { useState } from "react";
+import { IoClose } from "react-icons/io5";
+import { useUIStore } from "../../stores/uiStore";
+import emailjs from "@emailjs/browser";
+import { toast } from "react-toastify";
+import { useForm } from "react-hook-form";
+
+type FeedbackData = {
+  name: string;
+  email: string;
+  message: string;
+};
+
+export default function FeedbackModal() {
+  const [sending, setSending] = useState(false);
+  const { toggleFeedbackModal } = useUIStore();
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm<FeedbackData>();
+
+  const onSubmit = async (data: FeedbackData) => {
+    try {
+      setSending(true);
+      await emailjs.send(
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        data,
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+      );
+      toast.success("Feedback sent successfully!");
+      reset();
+      toggleFeedbackModal();
+    } catch (error) {
+      console.error(error);
+      toast.error("Failed to send feedback.");
+    } finally {
+      setSending(false);
+    }
+  };
+
   return (
-    <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[90%] lg:w-[48%] h-[65%] z-[99] rounded-[20px] shadow-lg dark:bg-[#303030] dark:text-white flex flex-col"></div>
+    <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[90%] md:w-[80%] lg:w-[40%] z-[999] rounded-[20px] shadow-lg dark:bg-[#303030] dark:text-white flex flex-col">
+      <div
+        onClick={toggleFeedbackModal}
+        className="absolute top-5 lg:top-6 right-5 lg:right-6 p-1 hover:bg-[#4a4a4a] text-[#9a9a9a] hover:text-[#f1f1f1] rounded-full cursor-pointer z-10 duration-100"
+      >
+        <IoClose className="w-6 h-6" />
+      </div>
+
+      <div className="w-full pl-7 py-6 text-lg lg:text-xl font-semibold">
+        Send Feedback
+      </div>
+
+      <hr className="my-1 mx-3 border-[#e5e7eb] dark:border-[#4a4a4a]" />
+
+      <div className="flex-1 overflow-y-auto p-2 md:p-6 space-y-6 custom_scrollbar">
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-3">
+          <div>
+            <input
+              type="text"
+              placeholder="Your name"
+              className="w-full p-3 bg-[#3a3a3a] hover:bg-[#3a3a3a]/80 focus:bg-[#3a3a3a]/80 text-white outline-none rounded-[16px] duration-300"
+              {...register("name", { required: "Name is required" })}
+            />
+            {errors.name && (
+              <p className="text-sm leading-none text-red-500 p-2">
+                {errors.name.message}
+              </p>
+            )}
+          </div>
+
+          <div>
+            <input
+              type="email"
+              placeholder="Your email"
+              className="w-full p-3 bg-[#3a3a3a] hover:bg-[#3a3a3a]/80 focus:bg-[#3a3a3a]/80 text-white outline-none rounded-[16px] duration-300"
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value: /^\S+@\S+$/i,
+                  message: "Enter a valid email",
+                },
+              })}
+            />
+            {errors.email && (
+              <p className="text-sm leading-none text-red-500 p-2">
+                {errors.email.message}
+              </p>
+            )}
+          </div>
+
+          <div>
+            <textarea
+              placeholder="Your message"
+              className="w-full h-[10rem] p-3 bg-[#3a3a3a] hover:bg-[#3a3a3a]/80 focus:bg-[#3a3a3a]/80 text-white outline-none resize-none rounded-[16px] duration-300"
+              {...register("message", { required: "Message is required" })}
+            />
+            {errors.message && (
+              <p className="text-sm leading-none text-red-500 p-2">
+                {errors.message.message}
+              </p>
+            )}
+          </div>
+
+          <div className="flex justify-between gap-3 mt-2">
+            <button
+              type="button"
+              onClick={toggleFeedbackModal}
+              className="w-[50%] border-none outline-none py-2 bg-red-400 hover:bg-red-400/60 active:bg-red-400/60 rounded-[16px] select-none duration-200"
+            >
+              Cancel
+            </button>
+
+            <button
+              type="submit"
+              disabled={sending}
+              className={`w-[50%] p-[10px] rounded-[16px] duration-200 select-none outline-none border-none ${
+                sending
+                  ? "bg-[#00bfff96] cursor-not-allowed"
+                  : "bg-[#00bfff] hover:bg-[#00bfff96] active:bg-[#00bfff63]"
+              }`}
+            >
+              {sending ? "Sending..." : "Send Feedback"}
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
--- frontend/src/components/Modals/NewFileModal.tsx
+++ frontend/src/components/Modals/NewFileModal.tsx
@@ -1,9 +1,9 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useFileStore } from "../../stores/fileStore";
 import { useUIStore } from "../../stores/uiStore";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
-import TypingTextAnimation from "../Animation/TypingTextAnimation";
+import { IoClose } from "react-icons/io5";
 
 interface NewFileInput {
   name: string;
@@ -57,82 +57,93 @@ const NewFileModal = () => {
   };
 
   return (
-    <div className="fixed z-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 dark:bg-[#121212] dark:text-white px-10 py-8 rounded-[40px] shadow-lg w-[90%] max-w-2xl overflow-hidden">
-      <h2 className="h-14 text-2xl text-center font-bold mb-6 font-mono">
-        <TypingTextAnimation text="Create New File" speed={60} />
-      </h2>
+    <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[90%] md:w-[80%] lg:w-[40%] z-[999] rounded-[20px] shadow-lg dark:bg-[#303030] dark:text-white flex flex-col">
+      <div
+        onClick={toggleNewFileModal}
+        className="absolute top-5 lg:top-6 right-5 lg:right-6 p-1 hover:bg-[#4a4a4a] text-[#9a9a9a] hover:text-[#f1f1f1] rounded-full cursor-pointer z-10 duration-100"
+      >
+        <IoClose className="w-6 h-6" />
+      </div>
 
-      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-3">
-        <div>
-          <input
-            type="text"
-            placeholder="Title"
-            className="w-full p-3 bg-[#333] hover:bg-[#333]/80 focus:bg-[#333]/80 text-white outline-none rounded-[16px] duration-300"
-            {...register("name", { required: "Filename is required" })}
-          />
+      <div className="w-full pl-7 py-6 text-lg lg:text-xl font-semibold">
+        Create New file
+      </div>
 
-          {errors.name && (
-            <p className="text-sm leading-none text-red-600 p-2">
-              {errors.name.message}
-            </p>
-          )}
+      <hr className="my-1 mx-3 border-[#e5e7eb] dark:border-[#4a4a4a]" />
 
-          {isDuplicate && (
-            <p className="text-sm leading-none text-red-600 p-2">
-              A file with this name already exists.
-            </p>
-          )}
-        </div>
+      <div className="flex-1 overflow-y-auto p-2 md:p-6 space-y-6 custom_scrollbar">
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-3">
+          <div>
+            <input
+              type="text"
+              placeholder="Title"
+              className="w-full p-3 bg-[#3a3a3a] hover:bg-[#3a3a3a]/80 focus:bg-[#3a3a3a]/80 text-white outline-none rounded-[16px] duration-300"
+              {...register("name", { required: "Filename is required" })}
+            />
 
-        <div>
-          <input
-            type="text"
-            placeholder="File type (e.g., js)"
-            className="w-full p-3 bg-[#333] hover:bg-[#333]/80 focus:bg-[#333]/80 text-white outline-none rounded-[16px] duration-300"
-            {...register("extension", { required: "Extension is required" })}
-          />
+            {errors.name && (
+              <p className="text-sm leading-none text-red-600 p-2">
+                {errors.name.message}
+              </p>
+            )}
 
-          {errors.extension && (
-            <p className="text-sm leading-none text-red-600 p-2">
-              {errors.extension.message}
-            </p>
-          )}
-        </div>
+            {isDuplicate && (
+              <p className="text-sm leading-none text-red-600 p-2">
+                A file with this name already exists.
+              </p>
+            )}
+          </div>
 
-        <div>
-          <textarea
-            placeholder="Description"
-            className="w-full h-[10rem] p-3 bg-[#333] hover:bg-[#333]/80 focus:bg-[#333]/80 text-white outline-none resize-none rounded-[16px] duration-300"
-            style={{
-              scrollbarWidth: "none",
-              msOverflowStyle: "none",
-            }}
-            {...register("description")}
-          />
-        </div>
+          <div>
+            <input
+              type="text"
+              placeholder="File type (e.g., js)"
+              className="w-full p-3 bg-[#3a3a3a] hover:bg-[#3a3a3a]/80 focus:bg-[#3a3a3a]/80 text-white outline-none rounded-[16px] duration-300"
+              {...register("extension", { required: "Extension is required" })}
+            />
 
-        <div className="flex justify-between gap-3 mt-2">
-          <button
-            type="button"
-            onClick={toggleNewFileModal}
-            className="w-[50%] border-none outline-none py-2 bg-red-400 hover:bg-red-400/60 active:bg-red-400/60 border rounded-[16px] select-none duration-200"
-          >
-            Cancel
-          </button>
+            {errors.extension && (
+              <p className="text-sm leading-none text-red-600 p-2">
+                {errors.extension.message}
+              </p>
+            )}
+          </div>
 
-          <button
-            type="submit"
-            disabled={creating || isDuplicate}
-            className={`w-[50%] p-[10px] rounded-[16px] duration-200 select-none outline-none border-none ${
-              creating || isDuplicate
-                ? "bg-[#00bfff96] cursor-not-allowed"
-                : "bg-[#00bfff] hover:bg-[#00bfff96] active:bg-[#00bfff63]"
-            }`}
-          >
-            {creating ? "Creating..." : "Create"}
-          </button>
-        </div>
-      </form>
+          <div>
+            <textarea
+              placeholder="Description"
+              className="w-full h-[10rem] p-3 bg-[#3a3a3a] hover:bg-[#3a3a3a]/80 focus:bg-[#3a3a3a]/80 text-white outline-none resize-none rounded-[16px] duration-300"
+              style={{
+                scrollbarWidth: "none",
+                msOverflowStyle: "none",
+              }}
+              {...register("description")}
+            />
+          </div>
+
+          <div className="flex justify-between gap-3 mt-2">
+            <button
+              type="button"
+              onClick={toggleNewFileModal}
+              className="w-[50%] border-none outline-none py-2 bg-red-400 hover:bg-red-400/60 active:bg-red-400/60 border rounded-[16px] select-none duration-200"
+            >
+              Cancel
+            </button>
+
+            <button
+              type="submit"
+              disabled={creating || isDuplicate}
+              className={`w-[50%] p-[10px] rounded-[16px] duration-200 select-none outline-none border-none ${
+                creating || isDuplicate
+                  ? "bg-[#00bfff96] cursor-not-allowed"
+                  : "bg-[#00bfff] hover:bg-[#00bfff96] active:bg-[#00bfff63]"
+              }`}
+            >
+              {creating ? "Creating..." : "Create"}
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
