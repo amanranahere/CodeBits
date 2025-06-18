fba74c8977483d618ada6b9683627ed15ddf1c7c
Aman Rana <amanranahere@gmail.com>
2025-06-19 01:46:40 +0530

    update app url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:8000/api/v1",
+      "/api": "https://codebits-backend.vercel.app/api/v1",
     },
   },
   plugins: [react()],
