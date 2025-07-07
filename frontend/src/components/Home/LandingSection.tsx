0c263a7a0374bd403e5d54cacef622e256c47001
Aman Rana <amanranahere@gmail.com>
2025-07-08 00:08:40 +0530

    add animations to landing section

--- frontend/src/components/Home/LandingSection.tsx
+++ frontend/src/components/Home/LandingSection.tsx
@@ -1,16 +1,30 @@
+import { motion } from "framer-motion";
+
 export default function LandingSection() {
   return (
-    <div className="h-screen flex flex-col justify-center items-center gap-y-8">
-      <div className="text-4xl md:text-6xl lg:text-7xl font-bold flex flex-col justify-center items-center text-center">
+    <motion.div className="h-full flex flex-col justify-center items-center gap-y-8">
+      <motion.div
+        initial={{ opacity: 0, y: -30 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        transition={{ duration: 0.5, ease: "easeOut", delay: 0.1 }}
+        className="text-4xl md:text-6xl lg:text-7xl font-bold flex flex-col justify-center items-center text-center"
+      >
         <span>The definitive hub</span>
         <span>for your code snippets</span>
-      </div>
+      </motion.div>
 
-      <p className="w-[90%] md:w-[70%] lg:w-[40%] md:text-lg text-[#bababa] font-semibold text-center">
+      <motion.p
+        initial={{ opacity: 0, y: -30 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        transition={{ duration: 0.5, ease: "easeOut", delay: 0.25 }}
+        className="w-[90%] md:w-[70%] lg:w-[40%] md:text-lg text-[#bababa] font-semibold text-center"
+      >
         Store and label snippets as you code. Find exactly what you need with
         one click. All your snippets live in a single reliable place so you can
         spend more time building and less time searching.
-      </p>
-    </div>
+      </motion.p>
+    </motion.div>
   );
 }
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -6,6 +6,6 @@ export default function Home() {
   const user = useUserStore((state) => state.user);
 
   return (
-    <div className="h-screen w-full">{user ? <HomeUser /> : <HomeGuest />}</div>
+    <div className="h-full w-full">{user ? <HomeUser /> : <HomeGuest />}</div>
   );
 }
