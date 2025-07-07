4bcc14c5b108a166683569e9152a8b97a662a199
Aman Rana <amanranahere@gmail.com>
2025-07-07 14:14:41 +0530

    add vid

--- frontend/src/components/Home/LandingSection.tsx
+++ frontend/src/components/Home/LandingSection.tsx
@@ -1,12 +1,12 @@
 export default function LandingSection() {
   return (
     <div className="h-screen flex flex-col justify-center items-center gap-y-8">
-      <div className="text-4xl md:text-6xl lg:text-7xl font-bold flex flex-col justify-center items-center">
+      <div className="text-4xl md:text-6xl lg:text-7xl font-bold flex flex-col justify-center items-center text-center">
         <span>The definitive hub</span>
         <span>for your code snippets</span>
       </div>
 
-      <p className="w-[80%] md:w-[70%] lg:w-[40%] md:text-lg text-[#bababa] font-semibold text-center">
+      <p className="w-[90%] md:w-[70%] lg:w-[40%] md:text-lg text-[#bababa] font-semibold text-center">
         Store and label snippets as you code. Find exactly what you need with
         one click. All your snippets live in a single reliable place so you can
         spend more time building and less time searching.
--- frontend/src/components/Home/SignupSection.tsx
+++ frontend/src/components/Home/SignupSection.tsx
@@ -4,22 +4,22 @@ export default function SignupSection() {
   const { toggleSignup, toggleLogin } = useUIStore();
 
   return (
-    <div className="h-screen flex flex-col justify-center items-center gap-y-10">
-      <div className="text-3xl md:text-5xl font-bold text-center flex flex-col justify-center items-center gap-y-2">
+    <div className="h-screen flex flex-col justify-center items-center gap-y-6 md:gap-y-10">
+      <div className="text-2xl md:text-5xl font-bold text-center flex flex-col justify-center items-center md:gap-y-2">
         <h1>If it's good enough to reuse,</h1>
         <h1>it's good enough to save.</h1>
       </div>
 
-      <div className="flex justify-center items-center gap-x-8">
+      <div className="flex justify-center items-center gap-x-4 md:gap-x-8">
         <button
           onClick={toggleSignup}
-          className="px-16 py-2 bg-[#1f1f1f] hover:bg-[#2a2a2a] active:bg-[#1a1a1a] rounded-full"
+          className="px-10 md:px-16 py-2 bg-[#1f1f1f] hover:bg-[#2a2a2a] active:bg-[#3a3a3a] rounded-full whitespace-nowrap"
         >
           Sign Up
         </button>
         <button
           onClick={toggleLogin}
-          className="px-16 py-2 bg-[#1f1f1f] hover:bg-[#2a2a2a] active:bg-[#1a1a1a] rounded-full"
+          className="px-10 md:px-16 py-2 bg-[#1f1f1f] hover:bg-[#2a2a2a] active:bg-[#3a3a3a] rounded-full whitespace-nowrap"
         >
           Log In
         </button>
